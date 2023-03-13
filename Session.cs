7e53ae019f8e2868c1bab3fe928ace3b68e23be6
T00226053 <liam.oconnor-collum@students.ittralee.ie>
2023-03-13 13:42:10 +0000

    fixed things

--- Session.cs
+++ Session.cs
@@ -14,6 +14,7 @@ namespace GymSYS
         private String className;
         private String classTeacher;
         private int classSize;
+        private int classReg;
         private String classDate;
         private int classDuration;
         private int classFee;
@@ -24,17 +25,19 @@ namespace GymSYS
             this.className = "";
             this.classTeacher = "";
             this.classSize = 0;
+            this.classReg = 0;
             this.classDate = Convert.ToString(DateTime.Today);
             this.classDuration = 0;
             this.classFee = 0;
         }
 
-        public Session(int classId, String className, String classTeacher, int classSize, String classDate, int classDuration, int classFee)
+        public Session(int classId, String className, String classTeacher, int classSize, int classReg, String classDate, int classDuration, int classFee)
         {
             this.classId = classId;
             this.className = className;
             this.classTeacher = classTeacher;
             this.classSize = classSize;
+            this.classReg = classReg;
             this.classDate = classDate;
             this.classDuration = classDuration;
             this.classFee = classFee;
@@ -55,6 +58,11 @@ namespace GymSYS
         public int getClassSize()
         {
             return this.classSize;
+        }
+
+        public int getClassReg()
+        {
+            return this.classReg;
         }
         public String getClassDate()
         {
@@ -85,6 +93,11 @@ namespace GymSYS
         {
             classSize = ClassSize;
         }
+
+        public void setClassReg(int ClassReg)
+        {
+            classReg = ClassReg;
+        }
         public void setClassDate(String ClassDate)
         {
             classDate = ClassDate;
@@ -108,7 +121,8 @@ namespace GymSYS
                 this.classId + ",'" +
                 this.className + "','" +
                 this.classTeacher + "'," +
-                this.classSize + ",'" +
+                this.classSize + "," +
+                this.classReg + ",'" +
                 this.classDate + "'," +
                 this.classDuration + "," +
                 this.classFee + ")";
@@ -134,6 +148,7 @@ namespace GymSYS
                 "Class_Name = '" + this.className + "'," +
                 "Class_Teacher = '" + this.classTeacher + "'," +
                 "Class_Size = " + this.classSize + "," +
+                "Class_Reg = " + this.classReg + ",'" +
                 "Class_Date = '" + this.classDate + "'," +
                 "Class_Duration = " + this.classDuration + "," +
                 "Class_Fee = " + this.classFee +
@@ -228,7 +243,7 @@ namespace GymSYS
             OracleConnection conn = new OracleConnection(DBConnect.oracledb);
 
             //define sql query to execute
-            String sqlQuery = "SELECT Class_Id,Class_Size,Class_Fee FROM Sessions ORDER BY Class_Id ASC";
+            String sqlQuery = "SELECT Class_Id,Class_Reg,Class_Fee FROM Sessions ORDER BY Class_Id ASC";
 
             //execute sql query
             OracleCommand cmd = new OracleCommand(sqlQuery, conn);
@@ -250,7 +265,7 @@ namespace GymSYS
             OracleConnection conn = new OracleConnection(DBConnect.oracledb);
 
             //define sql query to execute
-            String sqlQuery = "SELECT Class_Fee*Class_Size FROM Sessions";
+            String sqlQuery = "SELECT Class_Fee*Class_Reg FROM Sessions";
 
             //execute sql query
             OracleCommand cmd = new OracleCommand(sqlQuery, conn);
@@ -283,7 +298,7 @@ namespace GymSYS
             OracleConnection conn = new OracleConnection(DBConnect.oracledb);
 
             //define sql query to execute
-            String sqlQuery = "SELECT Class_Name,Class_Size FROM Sessions ORDER BY Class_Name ASC";
+            String sqlQuery = "SELECT Class_Name,Class_Reg FROM Sessions ORDER BY Class_Name ASC";
 
             //execute sql query
             OracleCommand cmd = new OracleCommand(sqlQuery, conn);
@@ -297,6 +312,49 @@ namespace GymSYS
             conn.Close();
 
             return ds;
+        }
+
+        public static int getNextRegistered()
+        {
+            //open a db connection
+            OracleConnection conn = new OracleConnection(DBConnect.oracledb);
+
+            //define sql query to execute
+            String sqlQuery = "SELECT Class_Size,Class_Reg FROM Sessions";
+
+            //execute sql query
+            OracleCommand cmd = new OracleCommand(sqlQuery, conn);
+            conn.Open();
+
+            OracleDataReader dr = cmd.ExecuteReader();
+
+            //is dr null
+            int classSize;
+            int regTotal;
+            dr.Read();
+
+            if (dr.IsDBNull(0))
+            {
+                regTotal = 0;
+            }
+            else
+            {
+                classSize = dr.GetInt32(3);
+                regTotal = dr.GetInt32(4);
+                if(regTotal == classSize)
+                {
+                    return classSize;
+                }
+                else
+                {
+                    regTotal = dr.GetInt32(4) + 1;
+                }
+            }
+
+            //close db connection
+            conn.Close();
+
+            return regTotal;
         }
     }
 }
