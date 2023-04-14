e2f983a2a2c96e01ae6efb9d0ca826c676eeb9a8
liam <liam.oconnor-collum@students.ittralee.ie>
2023-04-14 11:13:49 +0100

    refund

--- frmCancelBooking.cs
+++ frmCancelBooking.cs
@@ -126,14 +126,134 @@ namespace GymSYS
             cancelBooking.cancelBooking();
 
             //create session object
-            Session changeReg = new Session();
+            Session changeClass = new Session();
 
             //invoke method
-            changeReg.removeRegister();
+            changeClass.removeRegister();
 
             //Display Confirmation Message
             MessageBox.Show("Booking has cancelled successfully", "Success",
                 MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            //check current time against class time
+            int currentDateYear = Convert.ToInt32(DateTime.Now.ToString("yyyy"));
+            int currentDateMonth = Convert.ToInt32(DateTime.Now.ToString("M"));
+            int currentDateDay = Convert.ToInt32(DateTime.Now.ToString("dd"));
+
+            String classDate = changeClass.getClassDate();
+            Console.WriteLine(classDate);
+
+            int classDateMonth;
+
+            if (classDate.Contains("JAN"))
+            {
+                classDateMonth = 1;
+            }
+            else if (classDate.Contains("FEB"))
+            {
+                classDateMonth = 2;
+            }
+            else if (classDate.Contains("MAR"))
+            {
+                classDateMonth = 3;
+            }
+            else if (classDate.Contains("APR"))
+            {
+                classDateMonth = 4;
+            }
+            else if (classDate.Contains("MAY"))
+            {
+                classDateMonth = 5;
+            }
+            else if (classDate.Contains("JUN"))
+            {
+                classDateMonth = 6;
+            }
+            else if (classDate.Contains("JUL"))
+            {
+                classDateMonth = 7;
+            }
+            else if (classDate.Contains("AUG"))
+            {
+                classDateMonth = 8;
+            }
+            else if (classDate.Contains("SEP"))
+            {
+                classDateMonth = 9;
+            }
+            else if (classDate.Contains("OCT"))
+            {
+                classDateMonth = 10;
+            }
+            else if (classDate.Contains("NOV"))
+            {
+                classDateMonth = 11;
+            }
+            else if (classDate.Contains("DEC"))
+            {
+                classDateMonth = 12;
+            }
+            else
+            {
+                MessageBox.Show("Error retrieving the Class Date", "Error!",
+                MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (classDate.EndsWith(Convert.ToString(currentDateYear)) || classDate.EndsWith(Convert.ToString(currentDateYear + 1)))
+            {
+                if(currentDateMonth == classDateMonth)
+                {
+                    if (currentDateDay < Convert.ToInt32(classDate.Substring(0, 2)))
+                    {
+                        //include return of money
+                        MessageBox.Show("Payment has been refunded to account", "Success",
+                        MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Payment cannot be refunded as it is past the 24 hour refund time", "Error!",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+                    }
+                }
+                else if (currentDateMonth < classDateMonth)
+                {
+                    if (classDateMonth - currentDateMonth > 1)
+                    {
+                        //include return of money
+                        MessageBox.Show("Payment has been refunded to account", "Success",
+                        MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    else
+                    {
+                        if (currentDateDay != 1)
+                        {
+                            //include return of money
+                            MessageBox.Show("Payment has been refunded to account", "Success",
+                            MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        }
+                        else
+                        {
+                            MessageBox.Show("Payment cannot be refunded as it is past the 24 hour refund time", "Error!",
+                            MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            return;
+                        }
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Payment cannot be refunded as it is past the 24 hour refund time", "Error!",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Payment cannot be refunded as it is past the 24 hour refund time", "Error!",
+                MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
 
             //Reset UI
             cboBookingId.SelectedIndex = -1;
@@ -148,6 +268,72 @@ namespace GymSYS
             for (int i = 0; i < dsB.Tables[0].Rows.Count; i++)
             {
                 cboBookingId.Items.Add(dsB.Tables[0].Rows[i][0]);
+            }
+        }
+
+        private static void refundMoney()
+        {
+            //connect to database
+            OracleConnection conn = new OracleConnection(DBConnect.oracledb);
+
+            //create Booking object
+            Booking booking = new Booking();
+
+            //create all variables
+            int memberWallet = 0;
+            int memberPoints = 0;
+            int classFee = 0;
+
+            //sql query
+            String sqlQuery = "SELECT Member_Wallet,Member_Points FROM Members WHERE Member_Id = " + Convert.ToInt32(booking.getMemberId());
+
+            //execute query
+            OracleCommand cmd = new OracleCommand(sqlQuery, conn);
+            conn.Open();
+            OracleDataReader dr = cmd.ExecuteReader();
+            if (!dr.Read())
+            {
+                MessageBox.Show("There are no members found with that Booking ID");
+                return;
+            }
+            else
+            {
+                memberWallet = dr.GetInt32(0);
+                memberPoints = dr.GetInt32(1);
+            }
+
+            //create Class object
+            Session getClassFee = new Session();
+
+            //sql query
+            sqlQuery = "SELECT Class_Fee FROM Sessions WHERE Class_Id = " + Convert.ToInt32(booking.getClassId());
+
+            //execute query
+            cmd = new OracleCommand(sqlQuery, conn);
+            conn.Open();
+            dr = cmd.ExecuteReader();
+            if (!dr.Read())
+            {
+                MessageBox.Show("There are no classes found with that Booking ID");
+                return;
+            }
+            else
+            {
+                classFee = dr.GetInt32(0);
+            }
+
+            //create Class object
+            Member refundMember = new Member();
+
+            //change the data
+            //find a way to check what payment option was selected
+            if (true/*memberWallet is selected*/)
+            {
+                refundMember.setMemberWallet(memberWallet + classFee);
+            }
+            else
+            {
+                refundMember.setMemberPoints(memberPoints + classFee);
             }
         }
     }
