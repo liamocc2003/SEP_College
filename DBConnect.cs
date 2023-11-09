85caaba1a7d5f39830b9183033be46fa0ca6eb01
Liam <115699261+T00226053@users.noreply.github.com>
2023-11-09 18:41:52 +0000

    Alter details

--- DBConnect.cs
+++ DBConnect.cs
@@ -9,7 +9,7 @@ namespace GymSYS
     class DBConnect
     {
         //MTU Kerry Login:
-        public const String oracledb = "Data Source = oracle/orcl; User Id = T00226053; Password = mYg6_hqp39ub;";
+        public const String oracledb = "Data Source = oracle/orcl; User Id = ; Password = ";
 
         //Home Login:
         //public const String oracledb = "Data Source = localhost/orcl.students.ittralee.ie; User Id = ; Password = ;";
